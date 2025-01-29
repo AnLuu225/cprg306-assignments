7687d9c1a6761198eabe04d3bae8f8393c632cb7
AnLuu225 <An0919777317an@gmail.com>
2025-01-28 23:13:16 -0700

    Update page.js

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,13 @@
+import Link from 'next/link'
+
 export default function Page() {
     return (
       <main>
         <h1>Shopping List</h1>
+         <nav>
+            <p>Duc An Luu</p>
+            <Link href="/https://github.com/AnLuu225/cprg306-assignments">https://github.com/AnLuu225/cprg306-assignments</Link>
+        </nav>
       </main>
     );
   }
